87995d5f8c8b43403b6abe3e34ab0d6efb52ada5
Arief Yusron <arief.yusron@warungpintar.co>
2021-10-25 20:06:40 +0700

    upload csv

--- proses_file.php
+++ proses_file.php
@@ -1,31 +1,28 @@
 <?php
-// menggunakan class phpExcelReader
-include "excel_reader2.php";
+if (($open = fopen($_FILES['fileexcel']['tmp_name'], "r")) !== FALSE) 
+  {
+  
+    while (($data = fgetcsv($open, 1000, ",")) !== FALSE) 
+    {        
+      $array[] = $data; 
+    }
+  
+    fclose($open);
 
-$target = basename($_FILES['fileexcel']['name']);
-move_uploaded_file($_FILES['fileexcel']['tmp_name'], $target);
+    $queryDelete = "DELETE FROM data_hari";
+    $hasilDelete = mysqli_query($koneksi, $queryDelete);
 
-// beri permisi agar file xls dapat di baca
-chmod($_FILES['fileexcel']['name'],0777);
+    foreach ($array as $keyItem=>$item) {
+      if($keyItem > 0) {
+        $tanggal = $item[0]; 
+        $jumlah = $item[1];
+        $pendapatan = $item[2];
 
-$data = new Spreadsheet_Excel_Reader($_FILES['fileexcel']['tmp_name']);
-
-$baris = $data->rowcount($sheet_index=0);
-
-$berhasil = 0;
-
-for ($i=2; $i<=$baris; $i++)
-{
-  $tanggal = $data->val($i, 1); 
-  $jumlah = $data->val($i, 2);
-  $pendapatan = $data->val($i, 3);
-
-  if ($tanggal != "" && $jumlah != "" && $pendapatan != "") {
-    $query = "INSERT INTO data_hari VALUES (null, '$tanggal', $jumlah, $pendapatan)";
-    $hasil = mysqli_query($koneksi, $query);
-    $berhasil++;
+        if ($tanggal != "" && $jumlah != "" && $pendapatan != "") {
+          $query = "INSERT INTO data_hari VALUES (null, '$tanggal', $jumlah, $pendapatan)";
+          $hasil = mysqli_query($koneksi, $query);
+        }
+      }
+    }
   }
-}
-unlink($_FILES['fileexcel']['name']);
-header("location:data.php?berhasil=$berhasil");
 ?>
